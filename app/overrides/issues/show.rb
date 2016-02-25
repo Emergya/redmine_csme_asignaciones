fc84d1a9cc2d221998202361c4a273e613bbc3dc
manuabalos <manu.abalos@gmail.com>
2016-02-25 09:58:02 +0100

    Cambiado token deface en 'assigned_to_group_show' para Redmine 2.6.9

--- app/overrides/issues/show.rb
+++ app/overrides/issues/show.rb
@@ -1,5 +1,5 @@
 Deface::Override.new :virtual_path  => 'issues/show',
                      :name          => 'add_assigned_to_group_show',
-                     :original		=> 'db01b75572817be9b76234bdeb3fae80e5ce64a9',
+                     :original		=> '23afbd4cd0659d8ad1ceb024250d7ac289623fa4',
                      :replace       => "erb[loud]:contains(\"issue_fields_rows do |rows|\")",
                      :partial       => 'issues/partials/assigned_to_group_show'
